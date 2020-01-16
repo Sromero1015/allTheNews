0893111c523d181f83efe2fb62ac7f2e3528c86f
Sergio Romero <sromerojr1015@gmail.com>
2020-01-16 07:33:10 -0500

    test commit

--- models/Article.js
+++ models/Article.js
@@ -13,10 +13,7 @@ var ArticleSchema = new Schema({
     link: {
         type: String,
         required: true
-    },
-    // `note` is an object that stores a Note id
-    // The ref property links the ObjectId to the Note model
-    // This allows us to populate the Article with an associated Note
+    },    
     note: {
         type: Schema.Types.ObjectId,
         ref: "Note"
