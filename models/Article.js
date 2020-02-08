a8d1984a69531bbe4862fe9e12a7af9787dfb3bd
Sergio Romero <sromerojr1015@gmail.com>
2020-02-07 22:36:53 -0500

    fixing functions

--- models/Article.js
+++ models/Article.js
@@ -1,27 +1,21 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
-
 var ArticleSchema = new Schema({
-
     title: {
         type: String,
         required: true
     },
-    // `link` is required and of type String
     link: {
         type: String,
         required: true
-    },    
-    note: {
-        type: Schema.Types.ObjectId,
-        ref: "Note"
-    }
+    },
+    comment: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
 });
-
-// This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
-
-// Export the Article model
 module.exports = Article;
